3049abad66d78823dd465fe29bde7c2e3de53b35
David Gomez <74084216+davi0789@users.noreply.github.com>
2024-10-29 22:23:37 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
-import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
+    <Router basename="/landing-page"> {/* Agrega basename aquí */}
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/testimonials" element={<Testimonial />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
